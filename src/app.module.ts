a1816cee3f312a438903563b9519f39ab404710e
Giovane Silva <giovane.gomes94@hotmail.com>
2021-11-30 21:34:51 -0300

    Create Collaboratrs Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CollaboratorsModule } from './modules/collaborators/collaborators.module';
 
 @Module({
   imports: [GraphQLModule.forRoot({
     autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     sortSchema: true,
   }),
-  TypeOrmModule.forRoot()
+  TypeOrmModule.forRoot(),
+  CollaboratorsModule
   ],
   providers: [AppService],
 })
