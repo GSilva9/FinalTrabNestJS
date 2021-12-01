7bc26bafd1f18c8f1beda29b5478286e0fdc4107
Giovane Silva <giovane.gomes94@hotmail.com>
2021-11-30 21:22:13 -0300

    Project Base

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [GraphQLModule.forRoot({
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    sortSchema: true,
+  }),
+  TypeOrmModule.forRoot()
+  ],
+  providers: [AppService],
+})
+export class AppModule { }
