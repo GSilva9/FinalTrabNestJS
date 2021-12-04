f9c8b0a2a5c56e79170a6a8d4281ef67b3212e6a
Yuri da Roza <yurinobredaroza@gmail.com>
2021-12-04 00:03:35 -0300

    :sparkles: Add works(services) modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,19 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CollaboratorsModule } from './modules/collaborators/collaborators.module';
 import { TasksModule } from './modules/tasks/tasks.module';
+import { WorksModule } from './modules/works/works.module';
 
 @Module({
-  imports: [GraphQLModule.forRoot({
-    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    sortSchema: true,
-  }),
-  TypeOrmModule.forRoot(),
-  CollaboratorsModule,
-  TasksModule
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    TypeOrmModule.forRoot(),
+    CollaboratorsModule,
+    TasksModule,
+    WorksModule,
   ],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
